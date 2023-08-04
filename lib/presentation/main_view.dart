ae9e6cdc8c582bd3180a556ba5d1534c0fefa88b
dogukanozgurylmz <dogukanozgurylmz@gmail.com>
2023-08-04 15:25:24 +0300

    added header

--- lib/presentation/main_view.dart
+++ lib/presentation/main_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:neon_overflow/presentation/categories/categories_view.dart';
 import 'package:neon_overflow/presentation/home/home_view.dart';
+import 'package:neon_overflow/presentation/widgets/sidebar/header/header.dart';
 import 'package:neon_overflow/presentation/widgets/sidebar/sidebar.dart';
 import 'package:neon_overflow/presentation/widgets/sidebar/sidebar_provider.dart';
 import 'package:provider/provider.dart';
@@ -20,25 +21,30 @@ class MainView extends StatelessWidget {
               Responsive.isMobile(context)
                   ? const SizedBox.shrink()
                   : const Sidebar(),
-              Consumer<SidebarProvider>(
-                builder: (context, value, child) {
-                  var getStatus = context.read<SidebarProvider>().getStatus;
-                  if (getStatus == SidebarStatus.home) {
-                    return HomeView();
-                  } else if (getStatus == SidebarStatus.categories) {
-                    return const CategoriesView();
-                  } else if (getStatus == SidebarStatus.notifications) {
-                    return const Text("notification");
-                  } else if (getStatus == SidebarStatus.favorites) {
-                    return const Text("favorites");
-                  } else if (getStatus == SidebarStatus.myquestions) {
-                    return const Text("myquestions");
-                  } else if (getStatus == SidebarStatus.settings) {
-                    return const Text("settings");
-                  } else {
-                    return const Text("Default Data");
-                  }
-                },
+              Column(
+                children: [
+                  const Header(),
+                  Consumer<SidebarProvider>(
+                    builder: (context, value, child) {
+                      var getStatus = context.read<SidebarProvider>().getStatus;
+                      if (getStatus == SidebarStatus.home) {
+                        return HomeView();
+                      } else if (getStatus == SidebarStatus.categories) {
+                        return const CategoriesView();
+                      } else if (getStatus == SidebarStatus.notifications) {
+                        return const Text("notification");
+                      } else if (getStatus == SidebarStatus.favorites) {
+                        return const Text("favorites");
+                      } else if (getStatus == SidebarStatus.myquestions) {
+                        return const Text("myquestions");
+                      } else if (getStatus == SidebarStatus.settings) {
+                        return const Text("settings");
+                      } else {
+                        return const Text("Default Data");
+                      }
+                    },
+                  ),
+                ],
               )
             ],
           ),
